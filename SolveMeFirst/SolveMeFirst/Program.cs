f8d27359025991ea520d65ff3e32da0123747864
Richard S. Monfort <richard.s.monfort@gmail.com>
2015-03-04 14:53:18 -0500

    Added source code

---
+++ SolveMeFirst/SolveMeFirst/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SolveMeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value1 = Convert.ToInt32(Console.ReadLine());
+            int value2 = Convert.ToInt32(Console.ReadLine());
+            int sum = CalculateSum(value1, value2);
+            Console.WriteLine(sum);
+        }
+
+        private static int CalculateSum(int value1, int value2)
+        {
+            return value1 + value2;
+        }
+    }
+}
