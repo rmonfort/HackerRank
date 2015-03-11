5abc73212226a42c5b6864ea2c595e1d127e7ff5
Richard S. Monfort <richard.s.monfort@gmail.com>
2015-03-11 19:25:23 -0400

    Added MaxMin source

---
+++ MaxMin/MaxMin/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MaxMin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfIntegers = Convert.ToInt32(Console.ReadLine());
+            int numberOfIntegersToSelect = Convert.ToInt32(Console.ReadLine()); 
+            int[] arrayOfIntegers = new int[numberOfIntegers];
+
+            // Populate array of integers and sort it
+            for (int i = 0; i < numberOfIntegers; i++)
+            {
+                arrayOfIntegers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Array.Sort(arrayOfIntegers);
+
+            // Calculate the unfairness of each k-pair of numbers in array while retaining the smallest unfairness
+            int currentIndex = 0;
+            int adjacentIndex = numberOfIntegersToSelect - 1;
+            int? minUnfairness = null;
+            while (adjacentIndex != arrayOfIntegers.Length)
+            {
+                int tempUnfairness = arrayOfIntegers[adjacentIndex] - arrayOfIntegers[currentIndex];
+                if (minUnfairness == null || minUnfairness > tempUnfairness)
+                {
+                    minUnfairness = tempUnfairness;
+                }
+                currentIndex++;
+                adjacentIndex++;
+            }
+            Console.WriteLine(minUnfairness);
+        }
+    }
+}
