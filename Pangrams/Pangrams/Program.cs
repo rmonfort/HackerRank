7cacd7ff60ebb169ae17b16b4bc2ec0aa878cd4e
Richard S. Monfort <richard.s.monfort@gmail.com>
2015-03-14 08:41:59 -0400

    Added source for Pangrams

---
+++ Pangrams/Pangrams/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * https://www.hackerrank.com/challenges/pangrams
+ */
+
+using System;
+using System.Linq;
+
+namespace Pangrams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = Console.ReadLine();
+
+            // Test if sentence is pangram and output results
+            string result = IsPangram(sentence) ? "pangram" : "not pangram"; 
+            Console.WriteLine(result);
+        }
+
+        // Returns true if given sentence is pangram
+        private static bool IsPangram(string sentence)
+        {
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            return alphabet.All(sentence.ToLower().Contains);
+        }
+    }
+}
