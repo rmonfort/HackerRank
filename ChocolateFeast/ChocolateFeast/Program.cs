4e1ae36c10b389ffa13bcb0306ea72e863cd9451
Richard S. Monfort <richard.s.monfort@gmail.com>
2015-03-18 16:21:14 -0400

    Added source for Chocolate Feast and Sherlock and Squares

---
+++ ChocolateFeast/ChocolateFeast/Program.cs
@@ -0,0 +1,41 @@
+﻿/* 
+ * https://www.hackerrank.com/challenges/chocolate-feast
+ */
+
+using System;
+
+namespace ChocolateFeast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Obtain number of test cases and cycle through each one
+            int numberOfTestCases = Convert.ToInt32(Console.ReadLine());
+            for (int currentTestCase = 0; currentTestCase < numberOfTestCases; currentTestCase++)
+            {
+                // Split input of test case into pocket money, cost for each chocolate, and the number of wrappers needed to get a free chocolate
+                string input = Console.ReadLine();
+                string[] elements = input.Split(' ');
+                int pocketMoney = Convert.ToInt32(elements[0]);
+                int costPerChocolate = Convert.ToInt32(elements[1]);
+                int wrappersForFreeChocolate = Convert.ToInt32(elements[2]);
+
+                // Calculate number of chocolates that are purchaseable along with number of wrappers you'd get from them
+                int totalNumberOfChocolates = pocketMoney / costPerChocolate; 
+                int numberOfWrappers = totalNumberOfChocolates; 
+
+                // Apply the chocolate discount based on the current number of wrappers and the number of wrappers it takes to get a free chocolate
+                while (numberOfWrappers >= wrappersForFreeChocolate)
+                {
+                    numberOfWrappers -= wrappersForFreeChocolate; // Redeem wrappers for said chocolate
+                    totalNumberOfChocolates++; // Number of chocolate increases
+                    numberOfWrappers++; // Thus number of wrappers increases     
+                }
+
+                //Output
+                Console.WriteLine(totalNumberOfChocolates);
+            }
+        }
+    }
+}
---
+++ SherlockAndSquares/SherlockAndSquares/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SherlockAndSquares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfTestCases = Convert.ToInt32(Console.ReadLine());
+
+            // Cycle through each test case
+            for (int currentTestCase = 0; currentTestCase < numberOfTestCases; currentTestCase++)
+            {
+                // Parse input
+                string input = Console.ReadLine();
+                string[] elements = input.Split(' ');
+                int min = Convert.ToInt32(elements[0]);
+                int max = Convert.ToInt32(elements[1]);
+
+                // Output number of squares
+                int result = CountPerfectSquares(min, max);
+                Console.WriteLine(result);
+            }
+        }
+
+        // Returns the number of perfect squares between a and b inclusive
+        public static int CountPerfectSquares(int a, int b)
+        {
+            // Find the number of perfect squares between A & B (INCLUSIVE)
+            return (int)Math.Sqrt(b) - (int)Math.Sqrt(a - 1);
+        }
+
+    }
+}
