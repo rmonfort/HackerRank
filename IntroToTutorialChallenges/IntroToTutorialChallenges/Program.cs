f779ad837370a12779dbd0cb53a0a617befcb30a
Richard S. Monfort <richard.s.monfort@gmail.com>
2015-03-21 09:19:19 -0400

    Added source for Intro to Tutorial Challenges - Sorting

---
+++ IntroToTutorialChallenges/IntroToTutorialChallenges/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * https://www.hackerrank.com/challenges/tutorial-intro
+ */
+
+using System;
+using System.Linq;
+
+namespace IntroToTutorialChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valueToFind = Convert.ToInt32(Console.ReadLine());
+            int arraySize = Convert.ToInt32(Console.ReadLine());
+            string stringOfNumbers = Console.ReadLine();
+            int[] arrayOfNumbers = stringOfNumbers.Split(' ').Select(n => Convert.ToInt32(n)).ToArray(); // LINQ convert string to int array
+            Array.Sort(arrayOfNumbers);
+            int indexOfValue = Array.BinarySearch(arrayOfNumbers, valueToFind); // Do a binary search for the value to find
+            Console.WriteLine(indexOfValue);
+        }
+    }
+}
