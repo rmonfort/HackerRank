57d196def45a3fd1b849c8025e9174d9c2bef8df
Richard S. Monfort <richard.s.monfort@gmail.com>
2015-03-24 18:53:26 -0400

    Added source for FizzBuzz - LINQ ftw

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * https://www.hackerrank.com/challenges/fizzbuzz
+ */
+
+using System;
+using System.Linq;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enumerable.Range(1, 100).Select(
+                n =>
+                (n % 15 == 0) ? "FizzBuzz" :
+                (n % 3 == 0) ? "Fizz" :
+                (n % 5 == 0) ? "Buzz" :
+                n.ToString()).ToList().ForEach(Console.WriteLine);
+        }
+    }
+}
