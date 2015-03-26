23befb0e53a43aef6685cda78487f1f0743325f6
Richard S. Monfort <richard.s.monfort@gmail.com>
2015-03-26 17:14:25 -0400

    Added source for Alternating Characters

---
+++ AlternatingCharacters/AlternatingCharacters/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * https://www.hackerrank.com/challenges/alternating-characters
+ */
+
+using System;
+
+namespace AlternatingCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Cycle through each test case
+            int maxNumberOfTestCases = Convert.ToInt32(Console.ReadLine());
+            for (int currentTestCase = 0; currentTestCase < maxNumberOfTestCases; currentTestCase++)
+            {
+                string input = Console.ReadLine();
+
+                // Check input for consecutive characters, and record the min number of deletions for said string to not have consecutive characters
+                char prev = input[0];
+                int minNumberOfDeletions = 0;
+                for (int i = 1; i < input.Length; i++)
+                {
+                    if (input[i] == prev)
+                    {
+                        minNumberOfDeletions++;
+                    }
+                    else
+                    {
+                        prev = input[i];
+                    }
+                }
+                Console.WriteLine(minNumberOfDeletions);
+            }
+        }
+    }
+}
