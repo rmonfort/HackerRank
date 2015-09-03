23b4f03934b2b7f6b44de8be5e2a0f81b69c6410
Richard Monfort <r.monfort@cabem.com>
2015-09-03 10:35:35 -0400

    Added source for Diagonal Difference

---
+++ DiagonalDifference/DiagonalDifference/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * https://www.hackerrank.com/challenges/diagonal-difference
+*/
+
+using System;
+using System.Linq;
+
+namespace DiagonalDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dimension = Int32.Parse(Console.ReadLine());
+            int[] leftDiagonal = new int[dimension];
+            int[] rightDiagonal = new int[dimension];
+            for (int left = 0, right = dimension - 1; left < dimension; left++, right--)
+            {
+                int[] input = Console.ReadLine().Split(' ').Select(n => Int32.Parse(n)).ToArray();
+                leftDiagonal[left] = input[left];
+                rightDiagonal[right] = input[right];
+            }
+            int absoluteDifference = Math.Abs(leftDiagonal.Sum() - rightDiagonal.Sum());
+            Console.WriteLine(absoluteDifference);
+        }
+    }
+}
