548473ce5a2323b7145452caab640e0bec3e5ec7
r.monfort <r.monfort@cabem.com>
2015-09-03 08:45:04 -0400

    Added source for SimpleArraySum

---
+++ SimpleArraySum/SimpleArraySum/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace SimpleArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfInts = Int32.Parse(Console.ReadLine());
+            int[] numbers = Console.ReadLine().Split(' ').Select(n => Convert.ToInt32(n)).ToArray();
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
