c72c834251b6e5bb87d0e2a25311450eae4a4651
Richard Monfort <r.monfort@cabem.com>
2015-09-03 09:12:06 -0400

    Added for A Very Big Sum

---
+++ AVeryBigSum/AVeryBigSum/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace AVeryBigSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfInts = Int32.Parse(Console.ReadLine());
+            long[] numbers = Console.ReadLine().Split(' ').Select(n => Convert.ToInt64(n)).ToArray();
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
